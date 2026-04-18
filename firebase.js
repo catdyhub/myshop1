890ca552da89ebf3c19ebf62f137879bff917758
catdyhub <catdycatdy118@gmail.com>
2026-04-18 15:56:29 +0700

    Initialize Firebase configuration and services

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "infinityshop999.firebaseapp.com",
+  projectId: "infinityshop999",
+  storageBucket: "infinityshop999.firebasestorage.app",
+  messagingSenderId: "688652453993",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
